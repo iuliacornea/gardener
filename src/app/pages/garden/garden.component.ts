c9d4a0dac72a9d55e30da03d773a7bcb05f79ff6
iulia yoga <iulia.cornea5@gmail.com>
2020-08-23 12:47:08 +0300

    prettify add plant form

---
+++ src/app/pages/garden/garden.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gar-garden',
+  templateUrl: './garden.component.html',
+  styleUrls: ['./garden.component.css']
+})
+export class GardenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
