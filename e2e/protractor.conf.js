8a1dd26e86c48b4b59547af3c1a5b39c92d03e81
Florin Ionce <ionce.florin@gmail.com>
2020-09-01 22:49:05 +0300

    Add server base url env var

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: process.env.SERVER_BASE_URL || 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
